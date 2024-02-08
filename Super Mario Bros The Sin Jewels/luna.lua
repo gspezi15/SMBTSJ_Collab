ea8783813a4310351b201a6510ccb24f8bd7ddc2
aaron3b <159461059+aaron3b@users.noreply.github.com>
2024-02-08 19:43:01 +0100

    Add files via upload

--- Super Mario Bros The Sin Jewels/luna.lua
+++ Super Mario Bros The Sin Jewels/luna.lua
@@ -9,3 +9,11 @@ function onEvent(n)
         Text.showMessageBox("You wasted this one time press switch.")
     end
 end
+
+function onStart()
+    Graphics.setMainFramebufferSize(1280,720)
+end
+
+function onCameraUpdate()
+    camera.width,camera.height = Graphics.getMainFramebufferSize(1280,720)
+end
